309e5cee5266569efc5dfd65a41d25eb6e2ac3d1
Jessica <jessica.henry@ucdconnect.ie>
2025-02-16 19:26:38 +0000

    Just have Implemented the TrafficQueue class using LinkedList to simulate normal traffic

---
+++ SmartTrafficManagement/src/trafficmanagement/TrafficQueue.java
@@ -0,0 +1,29 @@
+package trafficmanagement;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TrafficQueue {
+    private Queue<String> trafficQueue;
+
+    public TrafficQueue() {
+        this.trafficQueue = new LinkedList<>();
+    }
+
+    public void enqueue(String vehicle) {
+        trafficQueue.add(vehicle);
+        System.out.println(vehicle + " added to the queue.");
+    }
+
+    public void dequeue() {
+        if (!trafficQueue.isEmpty()) {
+            System.out.println(trafficQueue.poll() + " passed the intersection.");
+        } else {
+            System.out.println("No vehicles in queue.");
+        }
+    }
+
+    public boolean isEmpty() {
+        return trafficQueue.isEmpty();
+    }
+}
