dcff1e77ffee66dec84034caadf8765cd1ae0330
Jessica.henry <jessica.henry@ucdconnect.ie>
2025-02-16 19:51:15 +0000

    Fixing queue bug not giving what expected

---
+++ SmartTrafficManagement/src/trafficmanagement/EmergencyTrafficQueue.java
@@ -0,0 +1,46 @@
+package trafficmanagement;
+
+// here we are implementing the priority queue
+
+import java.util.PriorityQueue;
+
+class Vehicle implements Comparable<Vehicle> {
+    String type;
+    int priority; // Higher priority for emergency vehicles
+
+    public Vehicle(String type, int priority) {
+        this.type = type;
+        this.priority = priority;
+    }
+
+    @Override
+    public int compareTo(Vehicle other) {
+        return Integer.compare(other.priority, this.priority); // Higher priority first
+    }
+
+    @Override
+    public String toString() {
+        return type;
+    }
+}
+
+public class EmergencyTrafficQueue {
+    private PriorityQueue<Vehicle> priorityQueue;
+
+    public EmergencyTrafficQueue() {
+        this.priorityQueue = new PriorityQueue<>();
+    }
+
+    public void enqueue(String vehicleType, int priority) {
+        priorityQueue.add(new Vehicle(vehicleType, priority));
+        System.out.println(vehicleType + " (priority " + priority + ") added to queue.");
+    }
+
+    public void dequeue() {
+        if (!priorityQueue.isEmpty()) {
+            System.out.println(priorityQueue.poll() + " passed first due to priority.");
+        } else {
+            System.out.println("No vehicles in queue.");
+        }
+    }
+}
