c254743c6d45cad612e06a4a5acd65cce27c7934
John Hughes <johnniallhughes@gmail.com>
2025-02-18 16:45:28 +0000

    Implemented the trafficRerouteList class

---
+++ SmartTrafficManagement/src/trafficmanagement/TrafficReRouteList.java
@@ -0,0 +1,20 @@
+package trafficmanagement;
+
+import java.util.ArrayList;
+
+public class TrafficReRouteList {
+    private final ArrayList<String> rerouteList = new ArrayList<>();
+
+    public void insertFront(String route) {
+        rerouteList.addFirst(route); // Insert at the front
+        System.out.println("New route added at front: " + route);
+    }
+
+    public void deleteRear() {
+        if (!rerouteList.isEmpty()) {
+            System.out.println("Old route removed: " + rerouteList.removeLast());
+        } else {
+            System.out.println("No old routes to remove.");
+        }
+    }
+}
